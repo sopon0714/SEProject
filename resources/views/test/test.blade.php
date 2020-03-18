fc84b63548602383f07659076bcebce7848aaf97
se62-08 <sopon0714@gmail.com>
2020-03-18 09:10:38 +0700

    layout

---
+++ resources/views/test/test.blade.php
@@ -0,0 +1,7 @@
+
+@extends('./layout/layout')
+@section('title',"test01")
+@section('Content')
+    <h4>tel 0625424005 </h4>
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/test', function () {
+    return view('test/test');
+});
 Route::get('/', function () {
     return view('welcome');
 });
