056739c61e0713ba9383bbbe125e704fd05df7a9
nunnararat <nunnararut.1999@gmail.com>
2020-03-24 23:35:37 +0700

    statics 2

--- resources/views/statics.blade.php
+++ resources/views/statics.blade.php
@@ -11,7 +11,7 @@
             <div class="col-sm-12" id="historyRequirements" style="overflow-y:auto;">
 
 
-            <!DOCTYPE html>
+            {{-- <!DOCTYPE html>
             <html lang="en">
                 <head>
                     <meta charset="utf-8">
@@ -68,10 +68,66 @@
 
 
                 </body>
+            </html> --}}
+
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="utf-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <title>ChartJs</title>
+
+            </head>
+
+            <body>
+
+
+                <canvas id="myChart" width="400" height="200"></canvas>
+
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.js"></script>
+                <script>
+                var ctx = document.getElementById("myChart");
+                        var myChart = new Chart(ctx, {
+                            //type: 'bar',
+                            //type: 'line',
+                            type: 'bar',
+                            data: {
+
+                                labels: ["เมาส์", "คีย์บอร์ด", "อุปกรณ์อิเล็กทรอนิกส์", "อุปกรณ์คอมพิวเตอร์"],
+                                datasets: [{
+                                    label: '# of Votes',
+                                    data: [12, 19, 3, 5],
+                                    backgroundColor: [
+                                        'rgba(255, 99, 132, 0.2)',
+                                        'rgba(54, 162, 235, 0.2)',
+                                        'rgba(255, 206, 86, 0.2)',
+                                        'rgba(75, 192, 192, 0.2)'
+                                    ],
+                                    borderColor: [
+                                        'rgba(255,99,132,1)',
+                                        'rgba(54, 162, 235, 1)',
+                                        'rgba(255, 206, 86, 1)',
+                                        'rgba(75, 192, 192, 1)'
+                                    ],
+
+                                    borderWidth: 2
+                                }]
+                            },
+                            options: {
+                                scales: {
+                                    yAxes: [{
+                                        ticks: {
+                                            beginAtZero:true
+                                        }
+                                    }]
+                                }
+                            }
+                        });
+                </script>
+
+            </body>
             </html>
-
-
-
 
             </div>
         </div>
