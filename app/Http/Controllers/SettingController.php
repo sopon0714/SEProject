0220862d30c6f5840f46e66da5b1c7dfaca8587d
Thnnnx <thanafonn@hotmail.com>
2020-03-25 20:38:52 +0700

    set5

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -12,18 +12,20 @@ class SettingController extends Controller
         $Setting = DB::select("SELECT * FROM `config`");
         return view("setting",["detail_setting" => $Setting]);
     }
-    // public function updateSetting(Request $req)
-    // {
-    //     $indexpageCategory = $this->indexpageCategory();
-    //     $config_key = $req->get('config_key');
-    //     $coonfig_value = $req->get('coonfig_value');
+    public function updateSetting(Request $req)
+    {
+        $day_update = $req->get('day_update');
+        $tel_update = $req->get('tel_update');
+        $email_update = $req->get('email_update');
+        $time_update = $req->get('time_update');
+        $config_key = $req->get('config_key');
+        $coonfig_value = $req->get('coonfig_value');
+            DB::table('config')
+            ->where('config_key', "day_cancelapprove")
+            ->update(['coonfig_value' => $day_update]);
 
-    //     for ($i=0; $i<count($Setting); $i++)
-    //     {
-    //         DB::table('config')
-    //         ->update(['{{$detail_setting[1]->coonfig_value}}'=>$day_update, 'tel'=>$tel_update, 'email'=>$email_update, 'time'=>$time_update]);
-    //     }
-
-    //     // return $this->indexpageCategory();
-    // }
+        // return $this->indexpageCategory();
+        // , '{{$detail_setting[1]->coonfig_value}}'=>$tel_update,
+        //     '{{$detail_setting[2]->coonfig_value}}'=>$email_update, '{{$detail_setting[3]->coonfig_value}}'=>$time_update
+    }
 }
