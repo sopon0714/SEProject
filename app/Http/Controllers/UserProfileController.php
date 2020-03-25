58658c444a43072fba9aa9ed55dc6eecc925d9af
minial <minial2541@gmail.com>
2020-03-25 16:24:34 +0700

    a

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -4,17 +4,19 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class UserProfileController extends Controller
 {
     public function getUser()
     {
-        $Username = "b6020501442";
+        $U = Session::get('USER');
+        $username = $U[0]->Username;
         $User = DB::select("SELECT user.UID, user.Title, user.FName, user.LName, user.GMail, usertype.UTName FROM user
-        INNER JOIN usertype ON user.UTID = usertype.UTID WHERE user.Username = '$Username'");
+        INNER JOIN usertype ON user.UTID = usertype.UTID WHERE user.Username = '$username'");
 
         $History = DB::select("SELECT requirement.ReqDate, requirement.RID, requirement.RStatus FROM requirement
-        INNER JOIN user ON user.UID = requirement.UID WHERE user.Username = '$Username'");
+        INNER JOIN user ON user.UID = requirement.UID WHERE user.Username = '$username'");
 
         return view("userProfile", ['User' =>  $User, 'history' => $History]);
     }
