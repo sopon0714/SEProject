1704d9abcd7d679fd7488a472a41aefa02d688ea
nunnararat <nunnararut.1999@gmail.com>
2020-03-26 00:53:18 +0700

    static

--- resources/views/statics.blade.php
+++ resources/views/statics.blade.php
@@ -32,19 +32,15 @@
                     // }
                 ?>
                 <script>
-                var ctx = document.getElementById("myChart");
-                        var myChart = new Chart(ctx, {
-                            type: 'bar',
-                            data: {
-                                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange", "Red", "Blue", "Yellow", "Green",],
-                                datasets: [{
-                                    label: '# of Votes',
-                                    data: [19, 17, 3, 5, 2, 3, 12, 19, 3, 5,],
-                                    // labels: <?=json_encode($labels)?>,
-                                    // datasets: [{
-                                    //     label: 'Report',
-                                    //     data: <?=json_encode($data, JSON_NUMERIC_CHECK);?>,
-                                    backgroundColor: [
+                    var ctx = document.getElementById("myChart");
+                    var myChart = new Chart(ctx, {
+                        type: 'bar',
+                        data: {
+                            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange", "Red", "Blue", "Yellow", "Green"],
+                            datasets: [{
+                                label: '# of Votes',
+                                data: [19, 17, 3, 5, 2, 3, 12, 19, 3, 5],
+                                backgroundColor: [
                                         'rgba(255, 99, 132, 0.2)',
                                         'rgba(54, 162, 235, 0.2)',
                                         'rgba(255, 206, 86, 0.2)',
@@ -55,8 +51,8 @@
                                         'rgba(54, 162, 235, 0.2)',
                                         'rgba(255, 206, 86, 0.2)',
                                         'rgba(75, 192, 192, 0.2)'
-                                    ],
-                                    borderColor: [
+                                ],
+                                borderColor: [
                                         'rgba(255,99,132,1)',
                                         'rgba(54, 162, 235, 1)',
                                         'rgba(255, 206, 86, 1)',
@@ -67,21 +63,21 @@
                                         'rgba(54, 162, 235, 1)',
                                         'rgba(255, 206, 86, 1)',
                                         'rgba(75, 192, 192, 1)'
-                                    ],
-                                    borderWidth: 2
+                                ],
+                                borderWidth: 1
+                            }]
+                        },
+                        options: {
+                            scales: {
+                                yAxes: [{
+                                    ticks: {
+                                        beginAtZero:true
+                                    }
                                 }]
-                            },
-                            options: {
-                                scales: {
-                                    yAxes: [{
-                                        ticks: {
-                                            beginAtZero:true
-                                        }
-                                    }]
-                                }
                             }
-                        });
-                </script>
+                        }
+                    });
+                    </script>
             </body>
             </html>
             </div>
